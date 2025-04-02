e7cf8fead227a12d28f8e0aa489aed754bc7dff8
Gabriel Trabuco <trabuco2206@gmail.com>
2025-04-01 22:23:27 -0300

    Att

--- AulaJS3/calculadoraBonita/Js/script.js
+++ AulaJS3/calculadoraBonita/Js/script.js
@@ -1,3 +1,16 @@
 function expressao(parametro){
     console.log(parametro);
+
+    document.getElementById('tela').value += parametro;
+}
+
+function resultado(){
+    
+    try {
+        let expressao = document.getElementById('tela').value;
+        let resultado = eval(expressao);
+        document.getElementById('tela').value = resultado;
+    } catch (error) {
+        document.getElementById('tela').value = "Error";
+    }
 }
