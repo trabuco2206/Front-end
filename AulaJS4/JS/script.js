b30cadb368b8a490b56f526604c932a26ea03e90
Gabriel Trabuco <trabuco2206@gmail.com>
2025-04-08 22:22:21 -0300

    Att

--- AulaJS4/JS/script.js
+++ AulaJS4/JS/script.js
@@ -1,11 +1,28 @@
 const botao = document.getElementById('add');
 const entrada = document.getElementById('entrada');
-const lista = document.getElementById('lista');
+const lista = document.getElementById('saida');
+const itens = []; //vetor
 
 botao.addEventListener('click', function(){
-    entrada.value = "Eu cliquei."
+    let valorEntrada = entrada.value;
+    itens.push(valorEntrada);
+    entrada.value = "";
+    console.log(itens);
+    saida();
 });
 
+function saida(){
+    lista.innerHTML = "";
+
+    for (let i = 0; i < itens.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = itens(i);
+        lista.appendChild(li);
+    }
+}
+
+/*
 botao.addEventListener('mouseover', function(){
-    entrada.value = 'Eu passei aqui...'
+    entrada.value = "Você passou em mim...";
 });
+*/
